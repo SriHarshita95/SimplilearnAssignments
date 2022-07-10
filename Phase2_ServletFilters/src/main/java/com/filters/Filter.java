123de7b96dc0487ca22fcb51c9308f70e539b87e
Sri Harshita Tata <sri.harshita95@gmail.com>
2022-07-11 02:21:36 +0530

    Servlet Filters

---
+++ Phase2_ServletFilters/src/main/java/com/filters/Filter.java
@@ -0,0 +1,59 @@
+package com.filters;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+/**
+ * Servlet Filter implementation class Filter
+ */
+@WebFilter(filterName="LoginFilter",urlPatterns={"/dashboard","/profile"})
+public class Filter extends HttpFilter implements javax.servlet.Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public Filter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		String userId=request.getParameter("userId");
+		if(userId!=null && userId.equals("johndoe")) {
+			// pass the request along the filter chain
+			chain.doFilter(request, response);
+		}
+		else {
+			PrintWriter pw=response.getWriter();
+			pw.println("Hey invalid user trying to access secure page....");
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ Phase2_ServletFilters/src/main/java/com/simplilearn/Profile.java
@@ -0,0 +1,41 @@
+package com.simplilearn;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Dashboard
+ */
+@WebServlet("/profile")
+public class Profile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Profile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().println("I am in Account Profile....");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
